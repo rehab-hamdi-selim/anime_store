29ecedb224e248072f9f5f05d0d4a8b5ba19b197
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-09-25 09:01:29 +0300

    app strings

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,4 @@
+const String homeLayoutScreen = "/";
+const String homeScreen = "/home_screen";
+const String charactersDetailsScreen = "/characters_details_screen";
+const String upgradePlanScreen = "/upgrade_plan_screen";
