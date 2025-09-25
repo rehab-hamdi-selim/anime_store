7177b043fabcc2c9092da1728abad0901b3244d1
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-09-25 08:57:00 +0300

    home/data

---
+++ lib/features/home/data/data_source/characters_dummy_data.dart
@@ -0,0 +1,29 @@
+import 'package:anime_store/features/home/data/models/characters.dart';
+
+List<CharacterModel> characters = [
+  CharacterModel(
+    image: 'assets/images/characters/character1.png',
+    title: 'Gon Freecss',
+    subTitle: 'Hunter x Hunter',
+  ),
+  CharacterModel(
+    image: 'assets/images/characters/character2.png',
+    title: 'Naruto Uzumaki',
+    subTitle: 'Naruto',
+  ),
+  CharacterModel(
+    image: 'assets/images/characters/character3.png',
+    title: 'Luffy',
+    subTitle: 'One Piece',
+  ),
+  CharacterModel(
+    image: 'assets/images/characters/character4.png',
+    title: 'Conan Edogawa',
+    subTitle: 'Detective Conan',
+  ),
+  CharacterModel(
+    image: 'assets/images/characters/character5.png',
+    title: 'Goku',
+    subTitle: 'Dragon Ball',
+  ),
+];
---
+++ lib/features/home/data/data_source/movie_dummy_data.dart
@@ -0,0 +1,28 @@
+import 'package:anime_store/features/home/data/models/movie.dart';
+
+List<MovieModel> movies = [
+  MovieModel(
+    image: 'assets/images/movies/movie1.png',
+    title: 'Hunter x Hunter',
+    subTitle: 'Adventure',
+    rate: 5.0,
+  ),
+  MovieModel(
+    image: 'assets/images/movies/movie4.png',
+    title: 'Demon slayer',
+    subTitle: 'Mystery',
+    rate: 5.0,
+  ),
+  MovieModel(
+    image: 'assets/images/movies/movie2.png',
+    title: 'Detective Conan',
+    subTitle: 'Mystery',
+    rate: 5.0,
+  ),
+  MovieModel(
+    image: 'assets/images/movies/movie3.png',
+    title: 'Dragon Ball ',
+    subTitle: 'Adventure',
+    rate: 5.0,
+  ),
+];
