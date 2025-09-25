ccd65b67d02266c5dcba9ddec3b9a43ad0b00fdf
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-09-25 08:38:52 +0300

    colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color appColor = Color(0XFF5436F8);
+  static const Color bottomNavigationBarIconsColor = Color(0XFFAEADB5);
+  static const Color myGrey = Color(0XFFA9A9A9);
+  static const Color dividerColor = Color(0XFF747272);
+}
